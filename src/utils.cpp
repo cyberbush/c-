53c81c1571f47fc16b3abeaba1883fb5e40b375f
David Bush <bush0386@vandals.uidaho.edu>
2022-02-01 18:53:04 -0800

    Initial utils

---
+++ src/utils.cpp
@@ -0,0 +1,6 @@
+//------------------ utils.cpp ------------------
+#include "utils.h"
+/*
+*   The utils file will be used for utility functions
+*   such as printing, building nodes, ect.
+*/
---
+++ src/utils.h
@@ -0,0 +1,9 @@
+//------------------ utils.h ------------------
+#ifndef _UTILS_H_
+#define _UTILS_H_
+/*
+*   The utils file will be used for utility functions
+*   such as printing, building nodes, ect.
+*/
+
+#endif
