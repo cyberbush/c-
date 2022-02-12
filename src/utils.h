db57141f05d2971fc51ff3bddd16efdad2b7b1df
David Bush <bush0386@vandals.uidaho.edu>
2022-02-11 17:16:53 -0800

    Moved functions around

--- src/utils.h
+++ src/utils.h
@@ -1,18 +1,9 @@
 //------------------ utils.h ------------------
 #ifndef _UTILS_H_
 #define _UTILS_H_
-/*
-*   The utils file will be used for utility functions
-*   such as printing, building nodes, ect.
-*/
 
-
-// Creating Node Functions
-static AST_Node* createNode(NodeKind);
-static AST_Node* createNodeFromToken(TokenData*, int);
-static AST_Node* createOpNode(const char*, int, AST_Node*, AST_Node*, AST_Node*);
-static AST_Node* createStmtNode(StmtKind, const char*, int, AST_Node*, AST_Node*, AST_Node*);
-static AST_Node* createDeclNode(DeclKind, ExpType, const char*, int, AST_Node*, AST_Node*, AST_Node*);
+// Function to relinquish memory from token
+static void removeToken(TokenData** tok);
 
 // Function to count number of arguments for a AST Tree node
 int countSiblings(AST_Node* t);
@@ -20,7 +11,5 @@ int countSiblings(AST_Node* t);
 AST_Node* getLastSibling(AST_Node* t);
 // Takes an ExpType and returns the corresponding string
 const char* ExpTypeToStr(ExpType type);
-
-
 
 #endif
