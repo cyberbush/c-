c25444532a5b2ee871fb88b0ea6e84deff507aa9
David Bush <bush0386@vandals.uidaho.edu>
2022-02-21 10:28:58 -0800

    Update printTree

--- src/printTree.cpp
+++ src/printTree.cpp
@@ -2,7 +2,7 @@
 
 #include "printTree.h"
 
-void printAST(AST_Node* root, int childNum, int level, bool annotated)
+void printAST(AST_Node* root, int childNum, int level, bool PFlag)
 {
     if(root == NULL) return;
     // Print the indention level
@@ -11,22 +11,22 @@ void printAST(AST_Node* root, int childNum, int level, bool annotated)
         printf(".   ");
     }
     if(childNum >= 0) printf("Child: %i  ", childNum);
-    printNode(root);  // Print root at same level
+    printNode(root, PFlag);  // Print root at same level
     printf(" [line: %i]\n", root->lineNum);
     for(int i = 0; i < 3; i++)  // Print everything below the child
     {
       if(root->child[i] != NULL){
-          printAST(root->child[i], i, level+1, annotated);
+          printAST(root->child[i], i, level+1, PFlag);
       }
     }
 
     if(root->sibling != NULL)
     {
-        printSiblingAST(root->sibling, 1, level, annotated); // Print the sibling tree
+        printSiblingAST(root->sibling, 1, level, PFlag); // Print the sibling tree
     }
 }
 
-void printSiblingAST(AST_Node* root, int siblingOrder, int level, bool annotated)
+void printSiblingAST(AST_Node* root, int siblingOrder, int level, bool PFlag)
 {
     // Print the indention level
     for(int i = 0; i < level; i++)
@@ -35,37 +35,37 @@ void printSiblingAST(AST_Node* root, int siblingOrder, int level, bool annotated
     }
     // print sibling index
     printf("Sibling: %i  ", siblingOrder);
-    printNode(root);  // Print root at same level
+    printNode(root, PFlag);  // Print root at same level
     printf(" [line: %i]\n", root->lineNum);
     for(int i = 0; i < 3; i++)  // Print everything below the child
     {
       if(root->child[i] != NULL){
-        printAST(root->child[i], i, level+1, annotated);
+        printAST(root->child[i], i, level+1, PFlag);
       }
     }
 
     if(root->sibling != NULL)
     {
-        printSiblingAST(root->sibling, ++siblingOrder, level, annotated); // Print the sibling tree
+        printSiblingAST(root->sibling, ++siblingOrder, level, PFlag); // Print the sibling tree
     }
 }
 
-void printNode(AST_Node* n)
+void printNode(AST_Node* n, bool PFlag)
 {
     if(n == NULL) return;
     switch(n->nodeKind)
     {
         case DeclK:
-            printDecl(n);
+            printDecl(n, PFlag);
             break;
         case StmtK:
-            printStmt(n);
+            printStmt(n, PFlag);
             break;
         case ExpK:
-            printExp(n);
+            printExp(n, PFlag);
             break;
         case TermK:
-            printTerm(n);
+            printTerm(n, PFlag);
             break;
         default:
             printf("Error: NodeKind undefined\n");
@@ -73,12 +73,13 @@ void printNode(AST_Node* n)
     }
 }
 
-void printTerm(AST_Node* n)
+void printTerm(AST_Node* n, bool PFlag)
 {
     switch(n->subkind.term)
     {
         case IDD:
-            printf("Id: %s", n->printName);
+            printf("Id: %s", n->name);
+            if(PFlag) { printf(" of type %s", ExpTypeToStr(n->expType));}
             break;
 
         default:
@@ -87,21 +88,31 @@ void printTerm(AST_Node* n)
     }
 }
 
-void printDecl(AST_Node* n)
+void printDecl(AST_Node* n, bool PFlag)
 {
+    string statc = n->isStatic ? "static " : "";
     switch(n->subkind.decl)
     {
         case VarK:
-            if(n->isArray) printf("Var: %s of array of type %s", n->printName, ExpTypeToStr(n->expType));
-            else if(n->isStatic) printf("Var: %s of static type %s", n->printName, ExpTypeToStr(n->expType));
-            else printf("Var: %s of type %s", n->printName, ExpTypeToStr(n->expType));
+            if(n->isArray) { 
+                if(PFlag) { printf("Var: %s is array of type %s", n->name, ExpTypeToStr(n->expType)); }
+                else { printf("Var: %s is array of type %s", n->name, ExpTypeToStr(n->expType)); }  
+            }
+            else if(n->isStatic) {
+                //if(PFlag) { printf("Var: %s of %stype %s", n->name, statc.c_str(), ExpTypeToStr(n->expType)); }
+                printf("Var: %s of type %s", n->name, ExpTypeToStr(n->expType));
+            }
+            else {
+                if(PFlag) { printf("Var: %s of type %s", n->name, ExpTypeToStr(n->expType)); }
+                else { printf("Var: %s of type %s", n->name, ExpTypeToStr(n->expType)); }
+            }
             break;        
         case FuncK:
-            printf("Func: %s returns type %s", n->printName, ExpTypeToStr(n->expType));
+            printf("Func: %s returns type %s", n->name, ExpTypeToStr(n->expType));
             break;
         case ParamK:
-            if(n->isArray) {printf("Parm: %s of array of type %s", n->printName, ExpTypeToStr(n->expType));}
-            else {printf("Parm: %s of type %s", n->printName, ExpTypeToStr(n->expType));}
+            if(n->isArray) {printf("Parm: %s of array of type %s", n->name, ExpTypeToStr(n->expType));}
+            else {printf("Parm: %s of type %s", n->name, ExpTypeToStr(n->expType));}
             break;
         default:
             printf("Error: unknown subtype of DECL\n");
@@ -109,7 +120,7 @@ void printDecl(AST_Node* n)
     }
 }
 
-void printStmt(AST_Node* n)
+void printStmt(AST_Node* n, bool PFlag)
 {
     switch(n->subkind.stmt)
     {
@@ -121,6 +132,7 @@ void printStmt(AST_Node* n)
             break;
         case CompoundK:
             printf("Compound");
+            //if(PFlag) { printMemloc(n, false); }
             break;
         case IfK:
             printf("If");
@@ -130,6 +142,7 @@ void printStmt(AST_Node* n)
             break;
         case ForK:
             printf("For");
+            //if(PFlag) { printf(" %s", to_string(n).c_str()); }
             break;
         case RangeK:
             printf("Range");
@@ -139,24 +152,32 @@ void printStmt(AST_Node* n)
     }
 }
 
-void printExp(AST_Node* n)
+void printExp(AST_Node* n, bool PFlag)
 {
+    string statc = n->isStatic ? "static " : "";
+    string array = n->isArray ? "is array " : "";
+    string mem_info = n->isArray ? to_string(n) : "";
     switch(n->subkind.exp)
     {
         case ConstantK:
-            printf("Const %s", n->printName);
+            if(PFlag) { printf("Const %s%s of type %s", array.c_str(), n->name, ExpTypeToStr(n->expType)); }
+            else { printf("Const %s", n->name); }
             break;
         case IdK:
-            printf("Id: %s", n->printName);
+            printf("Id: %s", n->name);
+            if(PFlag) { printf(" of type %s", ExpTypeToStr(n->expType)); }
             break;
         case AssignK:
-            printf("Assign: %s", n->printName);
+            printf("Assign: %s", n->name);
+            if(PFlag) { printf(" of type %s", ExpTypeToStr(n->expType)); }
             break;
         case OpK:
-            printf("Op: %s", n->printName);
+            printf("Op: %s", n->name);
+            if(PFlag) { printf(" of type %s", ExpTypeToStr(n->expType)); }
             break;
         case CallK:
-            printf("Call: %s", n->printName);
+            printf("Call: %s", n->name);
+            if(PFlag) { printf(" of type %s", ExpTypeToStr(n->expType)); }
             break;
         case InitK:
             break;
@@ -164,4 +185,10 @@ void printExp(AST_Node* n)
             printf("Error: Unknown ExpKind\n");
             break;
     }
+}
+
+// prints the string form of a node's memory information, add a newline if second param is true
+void printMemloc(AST_Node *n, bool newLine){
+    cout << to_string(n);
+    if(newLine) cout << "\n";
 }
--- src/printTree.h
+++ src/printTree.h
@@ -3,23 +3,26 @@
 #define _PRINTTREE_H_
 
 #include "AST_Node.h"
+#include "iostream"
 #include "utils.h"
 
 // Functions for printing the AST
 
 // Prints all nodes in the AST in prefix order
-static void printAST(AST_Node* root, int childNum, int level, bool annotated);
+static void printAST(AST_Node* root, int childNum, int level, bool PFlag);
 // Prints all nodes in the sibling AST 
-static void printSiblingAST(AST_Node* root, int siblingOrder, int level, bool annotated);
+static void printSiblingAST(AST_Node* root, int siblingOrder, int level, bool PFlag);
 // Used for printing a node, can be a TermK, DeclK, StmtK, or ExpK
-static void printNode(AST_Node* n);
+static void printNode(AST_Node* n, bool PFlag);
 // Used for printing terminals
-static void printTerm(AST_Node* n);
+static void printTerm(AST_Node* n, bool PFlag);
 // Used for printing Decl
-static void printDecl(AST_Node* n);
+static void printDecl(AST_Node* n, bool PFlag);
 // Used for printing Exp
-static void printExp(AST_Node* n);
+static void printExp(AST_Node* n, bool PFlag);
 // Used for printing Stmt
-static void printStmt(AST_Node* n);
- 
+static void printStmt(AST_Node* n, bool PFlag);
+
+void printMemloc(AST_Node *n, bool newLine);
+
 #endif
